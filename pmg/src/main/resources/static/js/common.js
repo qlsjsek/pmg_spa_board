40f71967e78009a31d8dd29762664da7df32305c
pmg <pmg@DESKTOP-SAUOLCO>
2024-04-05 16:53:52 +0900

    js 코드 분리

---
+++ pmg/src/main/resources/static/js/common.js
@@ -0,0 +1,32 @@
+function goToRegister() {
+	document.getElementById('loginPage').style.display = 'none';
+	document.getElementById('forgotUsernameForm').style.display = 'none';
+	document.getElementById('forgotPasswordForm').style.display = 'none';
+	document.getElementById('registerPage').style.display = 'block';
+}
+function goToLogin() {
+	document.getElementById('registerPage').style.display = 'none';
+		document.getElementById('forgotUsernameForm').style.display = 'none';
+	document.getElementById('forgotPasswordForm').style.display = 'none';
+	document.getElementById('loginPage').style.display = 'block';
+}
+function goToFindId() {
+	document.getElementById('loginPage').style.display = 'none';
+	document.getElementById('forgotUsernameForm').style.display = 'block';
+}
+function goToFindPassword() {
+	document.getElementById('loginPage').style.display = 'none';
+	document.getElementById('forgotPasswordForm').style.display = 'block';
+}
+function goToEditProfile() {
+	document.getElementById('profilePage').style.display = 'none';
+	document.getElementById('editProfilePage').style.display = 'block';
+}
+function goToBoardDetail(boardId) {
+	document.getElementById('boardListPage').style.display = 'none';
+	document.getElementById('boardDetailPage').style.display = 'block';
+}
+function goToEditBoard() {
+	document.getElementById('boardDetailPage').style.display = 'none';
+	document.getElementById('boardEditPage').style.display = 'block';
+}
