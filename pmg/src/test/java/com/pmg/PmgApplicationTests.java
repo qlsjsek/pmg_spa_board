b069934815bbf1225975e62ea7b085f3d8d4187d
pmg <pmg@DESKTOP-SAUOLCO>
2024-04-05 13:52:35 +0900

    클론확인

---
+++ pmg/src/test/java/com/pmg/PmgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
